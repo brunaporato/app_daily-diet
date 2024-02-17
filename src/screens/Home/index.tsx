871d3fc039fefe753bbeb1ad1fa73366db82ee46
Bruna <brunaporato@gmail.com>
2024-02-17 07:27:59 -0300

    refactor: change logic to organize meals by date and time

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -66,12 +66,6 @@ export function Home() {
         [],
       )
 
-      mealsByDateData.sort((a, b) => {
-        const dateA = new Date(a.date.split('.').reverse().join('-'))
-        const dateB = new Date(b.date.split('.').reverse().join('-'))
-        return dateB.getTime() - dateA.getTime()
-      })
-
       const { percentOfMealsOnDiet } = await getMealsData()
 
       setMealsByDate(mealsByDateData)
--- src/storage/meal/getMeals.ts
+++ src/storage/meal/getMeals.ts
@@ -9,6 +9,22 @@ export async function getMeals() {
 
     const meals: MealProps[] = storage ? JSON.parse(storage) : []
 
+    meals.sort((a, b) => {
+      const [dayA, monthA, yearA] = a.date.split('/').map(Number)
+      const [hourA, minuteA] = a.time.split(':').map(Number)
+      const dateA = new Date(yearA, monthA - 1, dayA, hourA, minuteA)
+
+      const [dayB, monthB, yearB] = b.date.split('/').map(Number)
+      const [hourB, minuteB] = b.time.split(':').map(Number)
+      const dateB = new Date(yearB, monthB - 1, dayB, hourB, minuteB)
+
+      if (dateB.getDate() === dateA.getDate()) {
+        return dateA.getTime() - dateB.getTime()
+      } else {
+        return dateB.getTime() - dateA.getTime()
+      }
+    })
+
     return meals
   } catch (error) {
     throw error
